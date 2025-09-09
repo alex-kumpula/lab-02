16fa3a7d212aeb394fe919c8b10fe3c00c2bd9d2
Alex Kumpula <alex.kumpula01@gmail.com>
2025-09-09 17:49:14 -0600

    Add delete city functionality

--- ListyCity/app/src/main/java/com/example/listycity/MainActivity.java
+++ ListyCity/app/src/main/java/com/example/listycity/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.listycity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 
 import androidx.activity.EdgeToEdge;
@@ -19,13 +24,38 @@ public class MainActivity extends AppCompatActivity {
     ListView cityList;
     ArrayAdapter<String> cityAdapter;
     ArrayList<String> dataList;
+    EditText cityEntryInputField;
+    Button cityEntryConfirmButton;
+    Button addCityButton;
+    Button deleteCityButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_PAN);
 
+        // Populate references from layout
         this.cityList = findViewById(R.id.city_list);
+        this.cityEntryInputField = findViewById(R.id.city_entry_input_field);
+        this.cityEntryConfirmButton = findViewById(R.id.city_entry_confirm_button);
+        this.addCityButton = findViewById(R.id.add_city_button);
+        this.deleteCityButton = findViewById(R.id.delete_city_button);
+
+        this.addCityButton.setOnClickListener(v -> {
+
+        });
+
+        this.deleteCityButton.setOnClickListener(v -> {
+            String selectedCity = (String)this.cityList.getItemAtPosition(this.cityList.getCheckedItemPosition());
+            if (selectedCity == null)
+                return;
+
+            this.cityAdapter.remove(selectedCity);
+            this.cityAdapter.notifyDataSetChanged();
+        });
+
+
 
         String[] cities = {
             "Edmonton",
