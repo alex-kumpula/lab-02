4d1a54f6b418f5d41783e10e923a1b6091be3e73
Alex Kumpula <alex.kumpula01@gmail.com>
2025-09-09 14:53:20 -0600

    ListyCity example implemented

--- ListyCity/app/src/main/java/com/example/listycity/MainActivity.java
+++ ListyCity/app/src/main/java/com/example/listycity/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.listycity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +10,40 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class MainActivity extends AppCompatActivity {
+
+    // References
+    ListView cityList;
+    ArrayAdapter<String> cityAdapter;
+    ArrayList<String> dataList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        this.cityList = findViewById(R.id.city_list);
+
+        String[] cities = {
+            "Edmonton",
+            "Vancouver",
+            "Moscow",
+            "Sydney",
+            "Berlin",
+            "Vienna",
+            "Tokyo",
+            "Beijing",
+            "Osaka",
+            "New Delhi"
+        };
+
+        this.dataList = new ArrayList<String>();
+        this.dataList.addAll(Arrays.asList(cities));
+
+        this.cityAdapter = new ArrayAdapter<String>(this, R.layout.content, this.dataList);
+        this.cityList.setAdapter(this.cityAdapter);
     }
 }
