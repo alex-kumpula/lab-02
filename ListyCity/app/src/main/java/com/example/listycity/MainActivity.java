baeb69bfb8b6c9441843cf603acd8a3ef96acde2
Alex Kumpula <alex.kumpula01@gmail.com>
2025-09-11 13:22:24 -0600

    Implement add city functionality

--- ListyCity/app/src/main/java/com/example/listycity/MainActivity.java
+++ ListyCity/app/src/main/java/com/example/listycity/MainActivity.java
@@ -1,18 +1,21 @@
 package com.example.listycity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
-import android.widget.AdapterView;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import java.util.ArrayList;
@@ -24,6 +27,7 @@ public class MainActivity extends AppCompatActivity {
     ListView cityList;
     ArrayAdapter<String> cityAdapter;
     ArrayList<String> dataList;
+    ConstraintLayout cityEntryLayout;
     EditText cityEntryInputField;
     Button cityEntryConfirmButton;
     Button addCityButton;
@@ -33,26 +37,90 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_PAN);
+
+        // Account for on-screen keyboard and other system insets.
+        // Referenced from: https://developer.android.com/develop/ui/views/layout/edge-to-edge#java
+        WindowCompat.setDecorFitsSystemWindows(getWindow(), false);
+        View root = findViewById(R.id.main);
+        ViewCompat.setOnApplyWindowInsetsListener(root, (v, insets) -> {
+            Insets imeInsets = insets.getInsets(WindowInsetsCompat.Type.ime());
+            Insets sysInsets = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+
+            // Add bottom padding equal to the on-screen keyboard height when it is visible
+            v.setPadding(
+                sysInsets.left,
+                sysInsets.top,
+                sysInsets.right,
+                imeInsets.bottom > 0 ? imeInsets.bottom : sysInsets.bottom
+            );
+
+            return WindowInsetsCompat.CONSUMED;
+        });
 
         // Populate references from layout
         this.cityList = findViewById(R.id.city_list);
+        this.cityEntryLayout = findViewById(R.id.city_entry_layout);
         this.cityEntryInputField = findViewById(R.id.city_entry_input_field);
         this.cityEntryConfirmButton = findViewById(R.id.city_entry_confirm_button);
         this.addCityButton = findViewById(R.id.add_city_button);
         this.deleteCityButton = findViewById(R.id.delete_city_button);
 
-        this.addCityButton.setOnClickListener(v -> {
+        // Hide the city entry layout at the start
+        this.cityEntryLayout.setVisibility(View.GONE);
 
+        this.addCityButton.setOnClickListener(v -> {
+            if (this.cityEntryLayout.getVisibility() == View.VISIBLE)
+            {
+                this.hideCityEntryLayout();
+            }
+            else
+                this.showCityEntryLayout();
         });
 
         this.deleteCityButton.setOnClickListener(v -> {
-            String selectedCity = (String)this.cityList.getItemAtPosition(this.cityList.getCheckedItemPosition());
+            // Guard against empty list
+            if (this.cityAdapter.isEmpty())
+                return;
+
+            // Gets the position in the city list of the selected city
+            int cityPositionInList = this.cityList.getCheckedItemPosition();
+
+            // Guard against getting an out-of-bounds position
+            if (cityPositionInList > this.cityList.getCount() - 1)
+                return;
+
+            // Gets the currently selected city in the list
+            String selectedCity = (String)this.cityList.getItemAtPosition(cityPositionInList);
+
+            // Guard in case there is no selected city
             if (selectedCity == null)
                 return;
 
+            // Remove the selected city from the list
             this.cityAdapter.remove(selectedCity);
             this.cityAdapter.notifyDataSetChanged();
+
+            // If selection is out of bounds, clamp it to bounds
+            if (cityPositionInList > this.cityList.getCount() - 1)
+            {
+                if (cityPositionInList > 0) {
+                    cityPositionInList -= 1;
+                    this.cityList.setItemChecked(cityPositionInList, true);
+                }
+                return;
+            }
+        });
+
+        this.cityEntryConfirmButton.setOnClickListener(v -> {
+            String cityName = this.cityEntryInputField.getEditableText().toString();
+
+            if (cityName.isBlank())
+                return;
+
+            this.cityAdapter.add(cityName);
+            this.cityAdapter.notifyDataSetChanged();
+
+            this.hideCityEntryLayout();
         });
 
 
@@ -75,5 +143,24 @@ public class MainActivity extends AppCompatActivity {
 
         this.cityAdapter = new ArrayAdapter<String>(this, R.layout.content, this.dataList);
         this.cityList.setAdapter(this.cityAdapter);
+    }
+
+    protected void showCityEntryLayout()
+    {
+        this.cityEntryInputField.setText("");
+        this.cityEntryLayout.setVisibility(View.VISIBLE);
+
+        // Focus the city input field and show the on-screen keyboard.
+        // Reference: https://developer.android.com/develop/ui/views/touch-and-input/keyboard-input/visibility
+        if (cityEntryInputField.requestFocus()) {
+            InputMethodManager imm = getSystemService(InputMethodManager.class);
+            imm.showSoftInput(cityEntryInputField, InputMethodManager.SHOW_IMPLICIT);
+        }
+    }
+
+    protected void hideCityEntryLayout()
+    {
+        this.cityEntryInputField.setText("");
+        this.cityEntryLayout.setVisibility(View.GONE);
     }
 }
--- README.md
+++ README.md
@@ -9,7 +9,9 @@
 
 List any resources used here, or simply put `N/A` if not applicable.
 
-`N/A`
+- `https://developer.android.com/develop/ui/views/layout/edge-to-edge#java` - For system insets, and accounting for the on-screen keyboard in the main activity layout.
+
+- `https://developer.android.com/develop/ui/views/touch-and-input/keyboard-input/visibility` - For focusing an input field and showing the on-screen keyboard programatically. Useful for when the "Add City" button is pressed.
 
 ## Verbal Collaboration
 
